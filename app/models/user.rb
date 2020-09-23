3d5ae35ef33753bfa1b6f761c6cf59f714ded69a
naok1207 <zenos.game.anime.all.vo1@gmail.com>
2020-09-23 17:22:23 +0900

    Userモデル修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,9 +16,9 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
 
-  validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
-  validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
-  validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
-
   validates :email, uniqueness: true
+
+  validates :password, presence: true, length: { minimum: 3 }
+  validates :password, confirmation: true
+  validates :password_confirmation, presence: true
 end
