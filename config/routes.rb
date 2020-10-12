986e52fb1d0ec7cc95809b3113a0da3116c3c0d2
naok1207 <zenos.game.anime.all.vo1@gmail.com>
2020-10-12 22:26:07 +0900

    非同期によるコメントCRUD機能を作成

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ Rails.application.routes.draw do
   delete '/logout',  to: "sessions#destroy"
 
   resources :users, only: %i[ show new create edit update destroy ]
-  resources :posts
-
+  resources :posts, shallow: true do
+    resources :comments
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
