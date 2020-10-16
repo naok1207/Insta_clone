1b3e2ee601e053c5ef05f86fbebeadf38128a46b
naok1207 <zenos.game.anime.all.vo1@gmail.com>
2020-10-17 06:26:45 +0900

    Likeモデル作成

--- app/models/post.rb
+++ app/models/post.rb
@@ -25,4 +25,7 @@ class Post < ApplicationRecord
     validates :body, presence: true, length: { maximum: 100 }
 
     has_many :comments, dependent: :destroy
+
+    has_many :likes, dependent: :destroy
+    has_many :like_users, through: :likes, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -26,4 +26,7 @@ class User < ApplicationRecord
 
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
+
+  has_many :likes, dependent: :destroy
+  has_many :like_posts, through: :likes, source: :post
 end
