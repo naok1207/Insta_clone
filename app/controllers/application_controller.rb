2893826e501db02a23741f7d12b8af1edec7e7e2
naok1207 <zenos.game.anime.all.vo1@gmail.com>
2020-11-05 02:34:09 +0900

    search機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
     before_action :require_login
     # before_action :set_locale
+    before_action :set_search_posts_form
 
     protected
 
@@ -8,6 +9,15 @@ class ApplicationController < ActionController::Base
         redirect_to login_path
     end
 
+    private
+
+    def set_search_posts_form
+        @search_form = SearchPostsForm.new(search_posts_params)
+    end
+
+    def search_posts_params
+        params.fetch(:q, {}).permit(:body)
+    end
     # def set_locale
     #     I18n.locale = current_user&.locale || :ja
     # end
