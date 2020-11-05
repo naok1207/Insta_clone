796b22c6bc13d57c53ed0936954711a329799d75
naok1207 <zenos.game.anime.all.vo1@gmail.com>
2020-11-06 07:44:15 +0900

    avatar default 設定 & 各imageのavatarを設定

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -20,6 +20,9 @@ class AvatarUploader < CarrierWave::Uploader::Base
   #
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
+  def default_url
+    'profile-placeholder.png'
+  end
 
   # Process files as they are uploaded:
   # process scale: [200, 300]
